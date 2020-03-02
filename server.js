b23d9f0be9c71ef83d1a0878a835894d661f06f1
Julien Sié <julien.sie@gmail.com>
2020-03-02 11:48:10 +0100

    update log message

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port = 9000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
